c99789d2811136f9077bb8a3d1e0245117f34298
Danila Lavrenov <danila.sept@mail.ru>
2022-04-26 21:19:56 +0300

    dz 1

---
+++ app.js
@@ -0,0 +1,76 @@
+/* 1 */
+// console.log(prompt('введите текст'));
+
+/* 2 */
+
+    /* let firstNumber = +prompt('Введите первое число');
+    let twoNumber = +prompt('Введите второе число');
+    let threeNumber = +prompt('Введите третье число');
+    
+    let plus = firstNumber + twoNumber + threeNumber;
+
+    if (typeof firstNumber === 'number' ) {
+        if (typeof twoNumber === 'number') {
+            if (typeof threeNumber === 'number') {
+                console.log (plus);  
+            }
+        }
+    }  */
+
+/* 3 */
+
+    /* let name = prompt('Введите свое имя');
+    let age = prompt('Введите свой возраст');
+    if (age >= 18 ) {
+        alert(`Добро пожаловать ${name}`);
+    } else {
+        alert(`Простите, ${name}, доступ закрыт`)
+    } */
+
+/* 4 */
+
+/* let questionBlock = confirm('Хотите ввести название блока?');
+if (questionBlock === true) {
+    let nameBlock = String (prompt('Введите название блока'))
+    let questionElement = confirm('Хотите ввести название элемента?');
+    if (questionElement === true) {
+        let nameElement = String (prompt('Введите название элемента'))
+        let questionModificator = confirm('Хотите ввести название модификатора?');
+        if (questionModificator === true) {
+            let nameModificator = prompt('Введите название модификатора')
+            alert(nameBlock + "__" + nameElement + "__" + nameModificator);
+        } else {
+            alert(nameBlock + "__" + nameElement + "__");
+        }
+    } else {
+        alert(nameBlock + "__" )
+    }
+} else {
+    alert('Название блока не введено')
+} */
+
+/* 5 */
+
+    /* let firstNumber = +prompt('Введите первое число');
+    let twoNumber = +prompt('Введите второе число');
+    
+    let plus = firstNumber + twoNumber;
+    let minus = firstNumber - twoNumber;
+    let mult = firstNumber * twoNumber;
+    let div = firstNumber / twoNumber;
+
+    if (typeof firstNumber === 'number' ) {
+        let operation = prompt('Введите операцию');
+        if (operation === "+") {
+            console.log (plus);
+        }
+        if (operation === "-") {
+            console.log (minus);
+        }
+        if (operation === "*") {
+            console.log (mult);
+        }
+        if (operation === "/") {
+            console.log (div);
+        }
+    } */
